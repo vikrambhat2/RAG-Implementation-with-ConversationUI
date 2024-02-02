57b50cfbe0b12341d51edabac78b5c78cf0a4f8c
Vikram Bhat <Vikram.Bhat1@ibm.com>
2024-02-02 15:46:38 +0000

    Update StreamlitApp.py

--- Streamlit Applications/StreamlitApp.py
+++ Streamlit Applications/StreamlitApp.py
@@ -125,7 +125,7 @@ def handle_userinput(user_question):
 def main():
     load_dotenv()
 
-    st.set_page_config(page_title="Chat with multiple PDFs",
+    st.set_page_config(page_title="Chat with your PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
